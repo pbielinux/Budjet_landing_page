8aaf5c0764e37da51541811a5b44e3052a3daa62
pbielinux <pedrobielik@hotmail.com>
2021-08-26 21:00:57 +0100

    Nav done; Adding mobile hero section

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,25 @@
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
-
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElemens = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', () => {
         console.log('Click Hamburger');
         
-        if (btnHamburger.classList.contains('open')){
-            btnHamburger.classList.remove('open');
-        } else {
-            btnHamburger.classList.add('open');
-        }
+        if (header.classList.contains('open')){ // Close Hamburger Menu
+            body.classList.remove('noscroll');
+            header.classList.remove('open');
+            fadeElemens.forEach((element) => {
+                element.classList.remove('fade-in');
+                element.classList.add('fade-out');
+            })
+        } else { // Open Hamburger Menu
+            body.classList.add('noscroll');
+            header.classList.add('open');
+            fadeElemens.forEach((element) => {
+                element.classList.remove('fade-out');
+                element.classList.add('fade-in');
+            })
+        } 
     });
