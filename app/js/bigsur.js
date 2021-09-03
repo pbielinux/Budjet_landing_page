2d02396c4e8696c3c2d48172cd35ea144fbf681d
Pedro henrique Bielik <pbielik@c2r2s6.42lisboa.com>
2021-09-03 22:05:45 +0100

    Hero remake

--- app/js/bigsur.js
+++ app/js/bigsur.js
@@ -1,51 +1,18 @@
-const { to, set, timeline, registerPlugin } = gsap;
+gsap.registerPlugin(ScrollTrigger);
 
-registerPlugin(ScrollTrigger);
+const tl = gsap.timeline({
+  scrollTrigger: {
+    id: "first",
+    trigger: ".section-hero",
+    markers: true,
+    start: "top top",
+    end: "center center",
+    scrub: 1
+  }
+});
 
-function initialiseElements() {
-    gsap.from(".hero-hardware", { transform: "scale(2.2)" });
-    gsap.from(".title-container", { opacity: 1 });
-}
 
-initialiseElements();
-
-const zoomTimeline = () => {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".outer",
-      pin: true,
-      scrub: true,
-      start: "start start",
-    }
-  });
-  tl.addLabel("start")
-    .to(".title-container", { opacity: 0 }, "start")
-    .to(".hero-hardware", {
-      transform: "scale(1)"
-    });
-  return tl;
-};
-
-const pinTimeline = () => {
-  const tl2 = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".first-super",
-      scrub: true,
-      start: 'bottom bottom',
-      end: '+=100',
-      // markers: true,
-      pin:'#text-block',
-      pinReparent:true,
-      onEnter:(scrollTrigger)=>{
-        scrollTrigger.refresh();
-      }
-    }
-  });
-  tl2.addLabel("pin-text")
-  .to('.intro-text__title', {opacity:1, delay:1})
-  return tl2;
-};
-
-const masterTimeline = gsap.timeline();
-masterTimeline.add(zoomTimeline);
-masterTimeline.add(pinTimeline);
+tl.to(".centered-text-block", { opacity: 0,  duration: 3 })
+.to(".hero-hardware", { transform: "matrix(1, 0, 0, 1, 0, 170)",  duration: 5 })
+.to(".image-app-placeholder", { transform: "matrix(1, 0, 0, 1, 0, 0)",  duration: 5, delay: -5 })
+  .to(".hero-hardware", { transform: "matrix(1, 0, 0, 1, 0, 0)",  duration: 5 });
