99432b339855a87c9347f60dd1335235e53959a2
pbielinux <pedrobielik@hotmail.com>
2021-09-07 20:42:58 +0100

    Section adjust; animate toggling classes

--- app/js/bigsur.js
+++ app/js/bigsur.js
@@ -1,29 +1,36 @@
+const features = document.querySelectorAll('.feature');
+
 gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline({
   scrollTrigger: {
     id: "hero",
     trigger: ".section-hero",
-    markers: false,
     start: "top top",
-    end: "center center",
-    scrub: 1
+    end: "center top",
+    scrub: 1,
+    markers: {
+      startColor: "purple",
+      endColor: "purple"
+    }
   }
 });
 
-tl.to(".centered-text-block", { opacity: 0,  duration: 3 })
+tl.to(".centered-text-block", { autoAlpha: 0,  duration: 3 })
 .to(".hero-hardware", { transform: "matrix(1, 0, 0, 1, 0, 170)",  duration: 5 })
 .to(".image-app-placeholder", { transform: "matrix(1, 0, 0, 1, 0, 0)",  duration: 5, delay: -5 })
   .to(".hero-hardware", { transform: "matrix(1, 0, 0, 1, 0, 0)",  duration: 5 });
 
 const tl2 = gsap.timeline({
   scrollTrigger: {
-    id: "section",
-    trigger: ".sticky-wrapper",
-    markers: true,
-    start: "top 10%",
-    end: "150",
-    scrub: 1
+    id: "hardware",
+    trigger: ".section-header",
+    start: "bottom top+=50",
+    scrub: 1,
+    markers: {
+      startColor: "lime",
+      endColor: "lime"
+    },
   }
 });
 
@@ -31,36 +38,28 @@ gsap.to(".image-sellers", 1, {left:'50%', xPercent:'-50'});
 
 tl2.to(".image-sellers", { transform: "matrix(0.7164, 0, 0, 0.7164, 440, 0)"});
 
-gsap.to("#feature-1", {
-  opacity: 1,
-  y: -40,
-  scrollTrigger: {
-    id: "feature-1",
-    trigger: ".image-hardware",
-    start: "top 10%",
-    end: "+=150",
-    toggleActions: "play reverse reverse play",
-    markers: {
-      startColor: "blue",
-      endColor: "blue",
-      fontSize: "1rem"
-    }
-  }
-});
 
-gsap.to("#feature-2", {
-  opacity: 1,
-  y: -40,
-  scrollTrigger: {
-    id: "feature-2",
-    trigger: "#feature-2",
-    start: "top 10%",
-    end: "+=150",
-    toggleActions: "play none reverse reverse",
+features.forEach((feature, index) => {
+  ScrollTrigger.create({
+    trigger: feature,
+    id: index+1,
+    duration: 0.8,
+    start: "top+=20 30%",
+    end: "bottom 30%",
+    toggleActions: "play reverse reverse reverse",
+    onEnter: () => {
+      feature.classList.add('js-feature-item-engaged');
+      feature.classList.add('js-feature-item-transition');
+    },
+    onLeave: () => {
+      feature.classList.remove('js-feature-item-engaged');
+      feature.classList.add('js-feature-item-transition');
+    },
     markers: {
-      startColor: "blue",
-      endColor: "blue",
-      fontSize: "1rem"
+      startColor: "fuchsia",
+      endColor: "fuchsia",
+      fontSize: "1rem",
+      indent: 100
     }
-  }
+  });
 });
